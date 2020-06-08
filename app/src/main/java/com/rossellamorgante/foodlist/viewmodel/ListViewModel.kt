2f3d0e322516f510e7155491c1fa2dbb35adcd37
Rossella Morgante <morgante.rossella@gmail.com>
2020-06-08 15:01:14 +0100

    ViewModel

--- app/src/main/java/com/rossellamorgante/foodlist/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/rossellamorgante/foodlist/viewmodel/ListViewModel.kt
@@ -1,6 +1,31 @@
 package com.rossellamorgante.foodlist.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.rossellamorgante.foodlist.model.Food
 
 class ListViewModel: ViewModel() {
+    val foods = MutableLiveData<List<Food>>()
+    val foodLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchFoods()
+    }
+    private fun fetchFoods(){
+        val mockData = listOf(Food("Pizza"),
+            Food("Spaghetti"),
+            Food("Burger"),
+            Food("Tiramisu"),
+            Food("Fish"),
+            Food("Chips"),
+            Food("Cake"),
+            Food("Muffin")
+        )
+        foodLoadError.value = false
+        loading.value = false
+        foods.value = mockData
+    }
+
+
 }
