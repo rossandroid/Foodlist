99eb1c81f4910cc5424fd843df68f169eafe57d8
Rossella Morgante <morgante.rossella@gmail.com>
2020-06-12 14:34:47 +0100

    Update Json

--- app/src/main/java/com/rossellamorgante/foodlist/model/Data.kt
+++ app/src/main/java/com/rossellamorgante/foodlist/model/Data.kt
@@ -5,6 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class Food(
     @SerializedName("foodName")
     val foodName: String?,
+    @SerializedName("type")
+    val type: String?,
     @SerializedName("foodpng")
     val picture: String?
 )
